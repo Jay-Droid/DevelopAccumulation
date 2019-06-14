a304440849bc0aa408516ea8f6030e77c53f41b2
王学杰 <xuejie.wang@cityio.cn>
2019-06-14 15:02:25 +0800

    git flow release publish v1.0

--- Kotlin/src/main/java/com/kotlin/MyKotlinClass.kt
+++ Kotlin/src/main/java/com/kotlin/MyKotlinClass.kt
@@ -5,6 +5,7 @@ fun main(args: Array<String>) {
     println("Hello Kotlin")
     println("git flow feature start feature1 ")
     println("git flow release start v1.0")
+    println("git flow release publish v1.0")
 
 
 }
